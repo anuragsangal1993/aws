45871db09801b56166c36bef53c73c1e153c62da
kiarapatel1975 <111404697+kiarapatel1975@users.noreply.github.com>
2022-10-02 12:58:25 +0530

    CMD of EBS beans modals

--- cli-examples.sh
+++ cli-examples.sh
@@ -29,7 +29,18 @@ aws dynamodb scan --table-name UserPosts --page-size 1
 # Max Item demo:
 aws dynamodb scan --table
 aws dynamodb scan --table-name UserPosts --max-items 1
-#!/bin/bash
+#!/bin/bash-name UserPosts --projection-expression "user_id, content"
+
+# Demo Filter Expression
+aws dynamodb scan --table-name UserPosts --filter-expression "user_id = :u" --expression-attribute-values '{ ":u": {"S":"john123"}}'
+
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
 
 aws dynamodb scan --table-name UserPosts --max-items 1
 #!/bin/bash
@@ -52,12 +63,7 @@ aws dynamodb scan --table-name UserPosts --page-size 1
 aws dynamodb scan --table-name UserPosts --max-items 1
 
 # Fetch the next item
-aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNsdXNpdmVTdGFydEtleSI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDF9
-
-# Fetch the next item
-aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNsdXNpdmVTdGFydEtleSI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDJ9
-
-# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb s1 API call if you have 3 Items
 aws dynamodb scan --tabl
 # Page Size demo: will do 1 API call if you have 3 Items
 aws dynamodb sca
